291dccc44f8a3b3f150c338eaf46d984ae697a2c
Fernando Tozato <101608124+Fernando-Tozato@users.noreply.github.com>
2024-05-03 16:02:11 -0300

    03/05

--- interno/urls.py
+++ interno/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import login, cadastro
+from .views import login, cadastro, matricula
 
 urlpatterns = [
     path('login/', login, name='login'),
     path('cadastro/', cadastro, name='cadastro'),
+    path('matricula/', matricula, name='matricula')
 ]
--- interno/views.py
+++ interno/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+def matricula(request):
+    return render(request, 'matricula.html')
+
 def login(request):
     return render(request, 'login.html')
 
