1f5f14249532994499f6d0dc0e9a73693eadc721
Fernando Tozato <101608124+Fernando-Tozato@users.noreply.github.com>
2024-08-18 14:17:31 -0300

    18/08

--- incubadora/settings.py
+++ incubadora/settings.py
@@ -98,12 +98,8 @@ AUTHENTICATION_BACKENDS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'incubadora',
-        'USER': 'admin',
-        'PASSWORD': '032daSilva58',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
