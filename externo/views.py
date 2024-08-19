b50ae56631093b12e59ae0bc0fd7992db8dafa07
Fernando Tozato <101608124+Fernando-Tozato@users.noreply.github.com>
2024-08-19 17:46:36 -0300

    19/08

--- externo/views.py
+++ externo/views.py
@@ -12,31 +12,35 @@ from io import BytesIO
 
 import os, zipfile, json
 
+
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'externo/home.html')
+
 
 def inscricao(request):
     agora = timezone.now()
     controle = Controle.objects.first()
-    inicio = timezone.localtime(controle.inscricao_inicio) # type: ignore
-    fim = timezone.localtime(controle.inscricao_fim) # type: ignore
-    remanescente = timezone.localtime(controle.matricula_geral) # type: ignore
-    
+    inicio = timezone.localtime(controle.inscricao_inicio)  # type: ignore
+    fim = timezone.localtime(controle.inscricao_fim)  # type: ignore
+    remanescente = timezone.localtime(controle.matricula_geral)  # type: ignore
+
     if agora < inicio:
         return render(request, 'antes_inscricao.html', {'data': inicio})
     elif agora > fim:
         return render(request, 'depois_inscricao.html', {'data_inscricao': fim, 'data_remanescente': remanescente})
     return render(request, 'inscricao.html')
 
+
 def enviado(request):
     return render(request, 'enviado_ext.html')
+
 
 @csrf_protect
 def validar_inscricao(request):
     if request.method == 'POST':
         try:
             data = json.loads(request.body)
-            
+
             nome = data['nome']
             nome_pesquisa = data['nome_pesquisa']
             nome_social = data['nome_social']
@@ -62,39 +66,39 @@ def validar_inscricao(request):
             pcd = data['pcd']
             ps = data['ps']
             id_turma = get_object_or_404(Turma, id=data['id_turma'])
-            
+
             print(pcd, type(pcd))
             print(ps, type(ps))
-            
+
             inscrito = Inscrito(
-                nome = nome,
-                nome_pesquisa = nome_pesquisa,
-                nome_social = nome_social if nome_social != '' else None,
-                nome_social_pesquisa = nome_social_pesquisa if nome_social_pesquisa != '' else None,
-                nascimento = nascimento,
-                cpf = cpf,
-                rg = rg if rg != '' else None,
-                data_emissao = data_emissao if data_emissao != '' else None,
-                orgao_emissor = orgao_emissor if orgao_emissor != '' else None,
-                uf_emissao = uf_emissao if uf_emissao != '' else None,
-                filiacao = filiacao,
-                escolaridade = escolaridade,
-                email = email if email != '' else None,
-                telefone = telefone if telefone != '' else None,
-                celular = celular if celular != '' else None,
-                cep = cep,
-                rua = rua,
-                numero = numero,
-                complemento = complemento if complemento != '' else None,
-                bairro = bairro,
-                cidade = cidade,
-                uf = uf,
-                pcd = pcd,
-                ps = ps,
+                nome=nome,
+                nome_pesquisa=nome_pesquisa,
+                nome_social=nome_social if nome_social != '' else None,
+                nome_social_pesquisa=nome_social_pesquisa if nome_social_pesquisa != '' else None,
+                nascimento=nascimento,
+                cpf=cpf,
+                rg=rg if rg != '' else None,
+                data_emissao=data_emissao if data_emissao != '' else None,
+                orgao_emissor=orgao_emissor if orgao_emissor != '' else None,
+                uf_emissao=uf_emissao if uf_emissao != '' else None,
+                filiacao=filiacao,
+                escolaridade=escolaridade,
+                email=email if email != '' else None,
+                telefone=telefone if telefone != '' else None,
+                celular=celular if celular != '' else None,
+                cep=cep,
+                rua=rua,
+                numero=numero,
+                complemento=complemento if complemento != '' else None,
+                bairro=bairro,
+                cidade=cidade,
+                uf=uf,
+                pcd=pcd,
+                ps=ps,
                 ja_sorteado=False,
-                id_turma = id_turma
+                id_turma=id_turma
             )
-            
+
             try:
                 inscrito.full_clean()
                 inscrito.save()
@@ -106,6 +110,7 @@ def validar_inscricao(request):
     else:
         return JsonResponse({'error': 'Método não permitido.'}, status=405)
 
+
 @csrf_protect
 def verificar_cpf(request):
     if request.method == 'POST':
@@ -113,7 +118,7 @@ def verificar_cpf(request):
             data = json.loads(request.body)
             cpf = data['cpf']
             inscrito = Inscrito.objects.filter(cpf=cpf)
-            
+
             if len(inscrito) == 0:
                 return JsonResponse({'response': False}, status=200)
             else:
@@ -123,6 +128,7 @@ def verificar_cpf(request):
     else:
         return JsonResponse({'error': 'Método não permitido.'}, status=405)
 
+
 @csrf_protect
 def busca_cursos(request):
     if request.method == 'POST':
@@ -130,14 +136,14 @@ def busca_cursos(request):
             data = json.loads(request.body)
             escolaridade = data['escolaridade']
             idade = data['idade']
-            
+
             turmas = Turma.objects.filter(Q(escolaridade__lte=escolaridade) & Q(idade__lte=idade))
-            
+
             cursos = []
             for turma in turmas:
                 if turma.curso not in cursos:
                     cursos.append(turma.curso)
-            
+
             if len(turmas) != 0:
                 return JsonResponse({'cursos': cursos}, status=200)
             else:
@@ -147,20 +153,21 @@ def busca_cursos(request):
     else:
         return JsonResponse({'error': 'Método não permitido.'}, status=405)
 
+
 @csrf_protect
 def busca_dias(request):
     if request.method == 'POST':
         try:
             data = json.loads(request.body)
             curso = data['curso']
-            
+
             turmas = Turma.objects.filter(curso=curso)
-            
+
             dias = []
             for turma in turmas:
                 if turma.dias not in dias:
                     dias.append(turma.dias)
-                    
+
             if len(turmas) != 0:
                 return JsonResponse({'dias': dias}, status=200)
             else:
@@ -170,6 +177,7 @@ def busca_dias(request):
     else:
         return JsonResponse({'error': 'Método não permitido.'}, status=405)
 
+
 @csrf_protect
 def busca_horarios(request):
     if request.method == 'POST':
@@ -177,9 +185,9 @@ def busca_horarios(request):
             data = json.loads(request.body)
             curso = data['curso']
             dias = data['dias']
-            
+
             turmas = Turma.objects.filter(Q(curso=curso) & Q(dias=dias))
-            
+
             horarios = []
             ids = []
             for turma in turmas:
@@ -189,7 +197,7 @@ def busca_horarios(request):
                 if final not in horarios:
                     horarios.append(final)
                     ids.append(turma.pk)
-                    
+
             if len(turmas) != 0:
                 return JsonResponse({'horarios': horarios, 'ids': ids}, status=200)
             else:
@@ -199,76 +207,91 @@ def busca_horarios(request):
     else:
         return JsonResponse({'error': 'Método não permitido.'}, status=405)
 
+
 def editais(request):
-    return render(request, 'editais.html')
+    return render(request, 'externo/editais.html')
+
 
 def resultado(request):
     agora = timezone.now()
     controle = Controle.objects.first()
-    sorteio = timezone.localtime(controle.sorteio_data) # type: ignore
-    
+    sorteio = timezone.localtime(controle.sorteio_data)  # type: ignore
+
     if agora < sorteio:
         return render(request, 'antes_resultado.html', {'data': sorteio})
-    
+
     cursos = Turma.objects.values_list('curso', flat=True).distinct()
-    return render(request, 'resultado.html', {'cursos': cursos})
+    return render(request, 'externo/resultado.html', {'cursos': cursos})
+
 
 def resultado_id(request, id_turma):
     agora = timezone.now()
     controle = Controle.objects.first()
-    sorteio = timezone.localtime(controle.sorteio_data) # type: ignore
-    
+    sorteio = timezone.localtime(controle.sorteio_data)  # type: ignore
+
     if agora < sorteio:
         return render(request, 'antes_resultado.html', {'data': sorteio})
-    
+
     cursos = Turma.objects.values_list('curso', flat=True).distinct()
     turma = get_object_or_404(Turma, id=id_turma)
     sorteados = Inscrito.objects.filter(Q(id_turma=turma) & Q(ja_sorteado=True))
-    
+
     busca = {
         'curso': turma.curso,
         'dias': turma.dias,
         'horario': turma.horario()
     }
-    
+
     if len(sorteados) == 0:
-        sorteados = -1   
-    
-    return render(request, 'resultado.html', {'cursos': cursos, 'sorteados': sorteados, 'busca': json.dumps(busca)})
+        sorteados = -1
+
+    return render(request, 'externo/resultado.html',
+                  {'cursos': cursos, 'sorteados': sorteados, 'busca': json.dumps(busca)})
+
 
 def design(request):
     return render(request, 'cursos/design.html')
 
+
 def educacao(request):
     return render(request, 'cursos/educacao.html')
+
 
 def excel(request):
     return render(request, 'cursos/excel.html')
 
+
 def gestao(request):
     return render(request, 'cursos/gestao.html')
+
 
 def info_basica(request):
     return render(request, 'cursos/info_basica.html')
 
+
 def info_melhor_idade(request):
     return render(request, 'cursos/info_melhor_idade.html')
+
 
 def marketing_digital(request):
     return render(request, 'cursos/marketing_digital.html')
 
+
 def marketing_emp(request):
     return render(request, 'cursos/marketing_emp.html')
+
 
 def montagem(request):
     return render(request, 'cursos/montagem.html')
 
+
 def robotica(request):
     return render(request, 'cursos/robotica.html')
 
+
 def download_validadores(request):
     planilhas_dir = os.path.join(settings.MEDIA_ROOT, 'sorteio')
-    
+
     # Verificar se a pasta "planilhas" existe
     if not os.path.exists(planilhas_dir):
         raise Http404("A pasta 'planilhas' não foi encontrada")
@@ -298,9 +321,10 @@ def download_validadores(request):
 
     return response
 
+
 def download_1(request):
     planilhas_dir = os.path.join(settings.MEDIA_ROOT, 'edital_2024_1')
-    
+
     # Verificar se a pasta "planilhas" existe
     if not os.path.exists(planilhas_dir):
         raise Http404("A pasta 'planilhas' não foi encontrada")
