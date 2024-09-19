30e286b463d35c3901797da1a4b6b7fa01c43bd7
Fernando Tozato <101608124+Fernando-Tozato@users.noreply.github.com>
2024-09-19 09:22:39 -0300

    modificação de email

--- interno/functions.py
+++ interno/functions.py
@@ -1,5 +1,12 @@
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMultiAlternatives
 from django.db.models import Q
+from django.template.loader import render_to_string
 from django.utils import timezone
+from django.utils.encoding import force_bytes
+from django.utils.html import strip_tags
+from django.utils.http import urlsafe_base64_encode
 
 from database.models import *
 
@@ -108,3 +115,22 @@ def verificar_inscritos(request, inscritos):
             return {'erro': 'O período de matrícula já terminou.'}
 
     return inscritos
+
+
+def enviar_email_senha(request, user):
+    subject = "Alteração de Senha"
+    email_template_name = "emails/password_reset_email.html"
+    c = {
+        "email": user.email,
+        'domain': get_current_site(request).domain,
+        'site_name': 'Incubadora de Robótica',
+        "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+        "user": user,
+        'token': default_token_generator.make_token(user),
+        'protocol': 'http',
+    }
+    email_content = render_to_string(email_template_name, c)
+    plain_message = strip_tags(email_content)
+    email_message = EmailMultiAlternatives(subject, plain_message, 'nao_responda@incubarobotica.com.br', [user.email])
+    email_message.attach_alternative(email_content, "text/html")
+    email_message.send()
