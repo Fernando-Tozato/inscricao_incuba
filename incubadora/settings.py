a50921edca75bf6732bbb9aa6fed9369c6ce2c95
Fernando Tozato <101608124+Fernando-Tozato@users.noreply.github.com>
2025-01-30 13:48:49 -0300

    home page courses pages (falta horarios centro)

--- incubadora/settings.py
+++ incubadora/settings.py
@@ -15,7 +15,6 @@ import os
 
 from incubadora import context_processors
 from decouple import config
-from sshtunnel import SSHTunnelForwarder
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
